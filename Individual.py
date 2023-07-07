e42a3fa82a1116a61d8ed424aaa5a26cae39da6e
BWang2010 <bella.y.zhi@gmail.com>
2023-07-07 20:01:43 +0100

    adding initial files

---
+++ Individual.py
@@ -0,0 +1,7 @@
+class Individual:
+    def __init__(self,target):
+        self.target = target
+        self.genetics = self.rand_genetics()
+
+    def rand_genetics(self):
+        return "hello world!"
---
+++ main.py
@@ -0,0 +1,6 @@
+from Individual import Individual
+
+if __name__ == "__main__":
+    target = "with enough probibility anything is possible"
+    individual = Individual(target)
+    print(individual.genetics)
