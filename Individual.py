477ca6303f5bcfa8eb0898affc1509a6e64df3af
BWang2010 <bella.y.zhi@gmail.com>
2023-07-17 08:28:36 +0100

    created code for random characters

--- Individual.py
+++ Individual.py
@@ -1,7 +1,12 @@
+import string
+import random
 class Individual:
     def __init__(self,target):
         self.target = target
         self.genetics = self.rand_genetics()
 
     def rand_genetics(self):
-        return "hello world!"
+        num = len(self.target)
+        for x in range(num):
+            randomLetter = random.choice(string.ascii_lowercase)
+            print(randomLetter)
